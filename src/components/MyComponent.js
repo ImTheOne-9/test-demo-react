c272defdd204ba03f3705028f553485f8e6b31f0
ImTheOne-9 <110813494+ImTheOne-9@users.noreply.github.com>
2025-02-05 15:00:02 +0700

    start

---
+++ src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>Hello world
+                {Math.random}
+            </div>
+        )
+    }
+}
+
+export default MyComponent;
